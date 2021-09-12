874ebf4fb9bc4388651410f087aa59a56e6a50f2
Candra <ecankurnia@gmail.com>
2021-09-12 23:06:40 +0700

    revision login screen

--- app/src/main/java/com/example/zyvashop/Onboarding.kt
+++ app/src/main/java/com/example/zyvashop/Onboarding.kt
@@ -3,21 +3,21 @@ package com.example.zyvashop
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class Onboarding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
 
-        val enter: Button = findViewById(R.id.button)
+        val enter: Button = findViewById(R.id.btn_email)
         enter.setOnClickListener {
-            intent = Intent(this,MainActivity::class.java)
+            intent = Intent(this,LoginEmail::class.java)
             startActivity(intent)
             finish()
         }
+
+
 
     }
 }
